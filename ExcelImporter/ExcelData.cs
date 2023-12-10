1121af18e430a18c39d35cd92490d13407093a1f
eldraco42 <108235392+eldraco42@users.noreply.github.com>
2023-12-10 18:32:34 +0100

    Added sample data.xlsx Added middlename to fields

--- ExcelImporter/ExcelData.cs
+++ ExcelImporter/ExcelData.cs
@@ -6,6 +6,7 @@ namespace ExcelImporter
     {
         [Description("nummer")] public double Number { get; set; }
         [Description("voornaam")] public string FirstName { get; set; }
+        [Description("tussenvoegsel")] public string MiddleName { get; set; }
         [Description("achternaam")] public string LastName { get; set; }
         [Description("geslacht")] public string Sex { get; set; }
     }
